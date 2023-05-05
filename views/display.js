8ad4e8234693370fdf174246345a84ad4bfb5d2e
saoirsemooney <saoirse.mooney@gmail.com>
2023-05-04 22:13:40 -0400

    cleaned up some stuff. logout buton

--- views/display.js
+++
@@ -1,9 +0,0 @@
-function displayCreate() {
-    let div = document.getElementById("create-new-task-block");
-    if (div.style.display === "none") {
-        div.style.display = "block";
-    } else {
-        div.style.display = "none";
-    }
-    
-}
